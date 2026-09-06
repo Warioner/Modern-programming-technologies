113203bc561949adaec32ba97288ee717ff5f018
Anton Muravev <anton.m3451@gmail.com>
2026-09-06 22:54:47 +0700

    Добавлены тесты 3 функции

--- lab1/test/test_order.cpp
+++ lab1/test/test_order.cpp
@@ -11,3 +11,9 @@ TEST(applyDiscount, test6) { EXPECT_EQ(applyDiscount(4000, false), 4000); }
 TEST(calcShipping, test1) { EXPECT_EQ(calcShipping(-5), -1.0); }
 TEST(calcShipping, test2) { EXPECT_EQ(calcShipping(7000), 0); }
 TEST(calcShipping, test3) { EXPECT_EQ(calcShipping(2000), 300); }
+
+TEST(finalPrice, test1) { EXPECT_DOUBLE_EQ(finalPrice(-5, 1, 1), -1.0); }
+TEST(finalPrice, test2) { EXPECT_DOUBLE_EQ(finalPrice(100, 1000, 100), 0.0); }
+TEST(finalPrice, test3) {
+  EXPECT_DOUBLE_EQ(finalPrice(1000, 100, 200), 1100.0);
+}
